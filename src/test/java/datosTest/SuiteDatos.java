127a817932dac21bab3ed0432b8e0a80c1bdadbc
Agustin-Perea <51423684+Agustin-Perea@users.noreply.github.com>
2023-10-24 18:40:57 -0300

    solucion de errores del proyecto

---
+++ src/test/java/datosTest/SuiteDatos.java
@@ -0,0 +1,11 @@
+package datosTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EmpleadorTest.class })
+public class SuiteDatos {
+
+}
