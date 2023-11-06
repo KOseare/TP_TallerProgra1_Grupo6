74dfbc564e10de92ad560d494ec69477277c9227
lmotylicki <leonardomotylicki@gmail.com>
2023-11-06 16:42:31 -0300

    dsa

--- src/test/java/persTest/PersistenciaTest.java
+++ src/test/java/persTest/PersistenciaTest.java
@@ -1,27 +1,66 @@
 package persTest;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import persistencia.AgenciaDTO;
+import persistencia.PersistenciaXML;
+
+/*void
+abrirInput(String nombre)
+ 
+void
+abrirOutput(String nombre)
+ 
+void
+cerrarInput()
+ 
+void
+cerrarOutput()
+ 
+void
+escribir(Object objeto)
+ 
+Object
+leer()*/
 public class PersistenciaTest {
-
+	AgenciaDTO ag;
+	PersistenciaXML persistencia;
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-
+	public void setUp() {
+		//borra el archivo
+		ag = new AgenciaDTO();
+		persistencia = new PersistenciaXML();
+		File arch = new File ("archivo.xml");
+		if (arch.exists())
+			arch.delete();
+		}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCrearArchivo() {
+		 //verifica que el mÃ©todo persistir crea un archivo
+		try {
+			File arch = new File ("archivo.xml");
+			Assert.assertTrue("debería existir el archivo archivo.xml", arch.exists());
+		}
+		catch (FileNotFoundException e) {
+			Assert.fail("No debería lanzar excepcion: "+e.getMessage());
+		}
 	}
+	@Test
+    public void testAbrirInput() throws IOException {
+        persistencia.abrirInput("archivo.xml");
+        
+    }
 
 }
