4c26ba42e89e018347b708858712549c80742d6e
agus2 <agus25050@hotmail.com>
2023-11-06 23:56:39 -0300

    update

--- src/test/java/TestGUI/GuiTestClientePanel.java
+++ src/test/java/TestGUI/GuiTestClientePanel.java
@@ -4,20 +4,20 @@ import static org.junit.Assert.fail;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.TextArea;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JList;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-import bin.modeloDatos.Ticket;
 import controlador.Controlador;
 import modeloDatos.Cliente;
-import modeloDatos.EmpleadoPretenso;
-import modeloDatos.Empleador;
 import modeloNegocio.Agencia;
 import util.Constantes;
 import vista.Ventana;
@@ -30,9 +30,9 @@ public class GuiTestClientePanel {
 	protected FalsoOptionPane op = new FalsoOptionPane();
 	
 	protected JButton loginButton,seleccionarCandidato,nuevoTicket,eliminarTicket,confirmarTicket,cerrarSesion;
-	protected JTextField nombreUsuario,contra;
-	protected JRadioButton jornadaMedia,jornadaCompleta,jornadaExtendida,expNada,expMedia,expMucha,primario,secundario,terciario,junior,senior,magnament,prescencial,homeOffice,indistinto;
-	protected JList<Ticket> textAreaTicket;
+	protected JTextField nombreUsuario,contra,remuneracionPretendida;
+	protected JRadioButton jornadaMedia,jornadaCompleta,jornadaExtendida,expNada,expMedia,expMucha,primario,secundario,terciario,junior,senior,managment,prescencial,homeOffice,indistinto;
+	protected JTextArea textAreaTicket;
 	protected JList<Cliente> listaCandidatos;
 	
 	public GuiTestClientePanel() {
@@ -96,13 +96,53 @@ public class GuiTestClientePanel {
 		jornadaExtendida = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
 				Constantes.JORNADA_EXTENDIDA);
 
+		expNada = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.EXP_NADA);
+		expMedia = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.EXP_MEDIA);
+		expMucha = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.EXP_MUCHA);
 		
-		textAreaTicket = (JList<Ticket>) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+		primario = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.PRIMARIOS);
+		secundario = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.SECUNDARIOS);
+		terciario = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.TERCIARIOS);
+		
+		junior = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.JUNIOR);
+		senior = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.SENIOR);
+		managment = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.MANAGMENT);
+		
+		prescencial = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.PRESENCIAL);
+		homeOffice = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.HOME_OFFICE);
+		indistinto = (JRadioButton) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.INDISTINTO);
+		
+		textAreaTicket = (JTextArea) TestUtils.getComponentForName((Ventana) controlador.getVista(),
 				Constantes.TEXT_AREA_TICKET);
 		listaCandidatos = (JList<Cliente>) TestUtils.getComponentForName((Ventana) controlador.getVista(),
 				Constantes.LISTA_CANDIDATOS);
-		
+		remuneracionPretendida = (JTextField) TestUtils.getComponentForName((Ventana) controlador.getVista(),
+				Constantes.TEXTFIELD_REMUNERACION);
 	}
 	
+	@Test
+	public void testSeleccionarCandidatoVacio() {
+		robot.delay(TestUtils.getDelay());
+		
+		try {
+			TestUtils.clickComponent(seleccionarCandidato, robot);	
+		}catch(Exception e) {
+			fail("no deberia lanzar excepcion");
+		}
+
+
+	}
 	
 }
