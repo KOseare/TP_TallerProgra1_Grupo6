3fb205697c9b485da92627fefb65ed7373ad84d6
lmotylicki <leonardomotylicki@gmail.com>
2023-11-10 19:01:34 -0300

    Contrataciones

---
+++ src/test/java/datosTest/ContratacionTest.java
@@ -0,0 +1,49 @@
+package datosTest;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import modeloDatos.Contratacion;
+import modeloDatos.EmpleadoPretenso;
+import modeloDatos.Empleador;
+import util.Constantes;
+
+public class ContratacionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+/*Contratacion(Empleador empleador, EmpleadoPretenso empleado)
+Construye una contratacion con el empleado y el empledor pasados por parametro
+ y la fecha actual pre: los parametros son diferentes de null*/
+	@Test
+	public void testContratacion() {
+		try {
+			Empleador empleador = new Empleador("neron","fire","Grupo LAH","0223 43332221",Constantes.COMERCIO_LOCAL,Constantes.FISICA);
+			EmpleadoPretenso empleado = new EmpleadoPretenso("watafilin","isbilivin","Juana","0223 1523412312","Morales",25);
+			Calendar fecha = new GregorianCalendar();
+			Contratacion c = new Contratacion(empleador,empleado);
+			assertEquals("Error en el parámetro Empleador", empleador, c.getEmpleador());
+			assertEquals("Error en el parámetro EmpleadoPretenso", empleado, c.getEmpleado());
+			assertTrue("Error en el año del atributo fecha",fecha.get(Calendar.YEAR)==c.getFecha().get(Calendar.YEAR));
+			assertTrue("Error en el mes del atributo fecha",fecha.get(Calendar.MONTH)==c.getFecha().get(Calendar.MONTH));
+			assertTrue("Error en el día del atributo fecha",fecha.get(Calendar.DATE)==c.getFecha().get(Calendar.DATE));
+			
+		}
+		catch (Exception e) {
+			fail("No debería lanzar excepción");
+		}
+		
+	}
+
+}
